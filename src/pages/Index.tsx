96ee40e44ae856be3b715eca1c19fc866f73dcbf
Muaaviya <muaaviyaansari007@gmail.com>
2026-01-12 22:35:52 +0530

    Added pricing section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,20 +4,22 @@ import LogoCloudSection from "@/components/landing/LogoCloudSection";
 import FeaturesSection from "@/components/landing/FeaturesSection";
 import CTASection from "@/components/landing/CTASection";
 import Footer from "@/components/landing/Footer";
+import PricingSection from "@/components/landing/PricingSection";
 
 const Index = () => {
-  return (
-    <div className="min-h-screen bg-background">
-      <Header />
-      <main>
-        <HeroSection />
-        <LogoCloudSection />
-        <FeaturesSection />
-        <CTASection />
-      </main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-background">
+            <Header />
+            <main>
+                <HeroSection />
+                <LogoCloudSection />
+                <FeaturesSection />
+                <PricingSection />
+                <CTASection />
+            </main>
+            <Footer />
+        </div>
+    );
 };
 
 export default Index;
